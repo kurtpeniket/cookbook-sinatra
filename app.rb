b69a6eddedf401c332b1c7a2897f3dba8285c1cd
Kurt Peniket <kurt.peniket@gmail.com>
2021-01-25 16:36:59 +0000

    new view files

--- app.rb
+++ app.rb
@@ -6,7 +6,18 @@ configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
+set :bind, '0.0.0.0'
 
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  binding.pry
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
 end
